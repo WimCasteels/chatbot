e1ab6900b422864566fc8d235ea95806cb88e84b
Wim Casteels <67267294+WimCasteels@users.noreply.github.com>
2025-09-24 13:45:22 +0000

    improve chatbot

--- Start.py
+++ Start.py
@@ -1,9 +1,6 @@
 import streamlit as st
 from openai import OpenAI
-from google import genai
 import os
-from google.genai import types
-from openai import OpenAI
 
 
 st.set_page_config(
@@ -15,49 +12,77 @@ st.set_page_config(
 # Show title and description.
 st.title("🌱 Sustainable AI")
 
-with st.sidebar:
-    st.text("Jou (leer)profiel:")
-    if "profiel" in st.session_state:
-        st.markdown(st.session_state.profiel)
+#with st.sidebar:
+#    if "profiel" in st.session_state:
+#        st.markdown(st.session_state.profiel)
 
-# openai_api_key = st.text_input("OpenAI API Key", type="password")
-google_key = os.getenv('GOOGLE_API')
-OR_key = os.getenv('OPEN_ROUTER_API')
+if "client" not in st.session_state:
+  OR_key = os.getenv('OPEN_ROUTER_API')
+  st.session_state.client = OpenAI(
+    base_url="https://openrouter.ai/api/v1",
+    api_key=OR_key
+  )
 
-
-client = OpenAI(
-  base_url="https://openrouter.ai/api/v1",
-  api_key=OR_key
-)
-
-completion = client.chat.completions.create(
-  model="x-ai/grok-4-fast:free",
-  messages=[
-    {
-      "role": "user",
-      "content": "What is the meaning of life?"
-    }
-  ]
-)
+#completion = client.chat.completions.create(
+#  model="x-ai/grok-4-fast:free",
+#  messages=[
+#    {
+#      "role": "user",
+#      "content": "What is the meaning of life?"
+#    }
+#  ]
+#)
 # st.write(completion.choices[0].message.content)
 
 if "messages" not in st.session_state:
-    st.session_state.messages = [{'role': 'system', 'content': 'You are a helpful assistant.'}]
+    system_instruction="""Je bent een vriendelijke chatbot in Vlaanderen die een kort en informeel startgesprek heeft met de gebruiker voordat de gebruiker start met de leermodules over AI.
+                    Pas je wat betreft de taal aan aan de gebruiker. 
+                    Je doel is om snel een beeld te vormen van de gebruiker, zonder dat het voelt als een interview. 
+                    Stuur het gesprek naar:
+                    - Aanspreking (naam, bijnaam).
+                    - Leeftijd en opleidingsniveau (bijv. lager, middelbaar, universitair).
+                    - Taalvaardigheid (moedertaal, voorkeur voor instructietaal).
+                    - Reden om deel te nemen (werkgerelateerd, verplicht voor studie, persoonlijke interesse).
+                    - Digitale geletterdheid (beginner, gemiddeld, gevorderd).
+                    - Voorkeursleerstijl (tekst lezen, video, audio/podcast, interactieve oefeningen).
+                    - Huidige kennisniveau van AI (geen, basis, gemiddeld, gevorderd).
+                    - Ervaring met AI-tools (bijv. ChatGPT, DALL·E, Midjourney, Copilot, AI in MS Office/Google).
+                    - Houding tegenover AI: nieuwsgierig, sceptisch, kritisch, enthousiast.
+                    - Specifieke interesses (Praktisch gebruik, Technisch, Ethisch/juridisch, Toekomst, ...)
+
+                    Hou je reacties kort, vriendelijk en menselijk. 
+                    Vermijd formele testvragen of opsommingen; laat het voelen als een natuurlijk gesprek.
+                    Vraag niet te hard door, ga op tijd over naar een ander onderwerp.
+                    Wijk niet af van het onderwerp. Keer terug naar het onderwerp als de gebruiker iets ander vraagt of zegt.
+                    Presenteer jezelf niet als een mens."""
+    
+
+    openingszin = st.session_state.client.chat.completions.create(
+            model="x-ai/grok-4-fast:free",
+            messages=[{'role': 'system', 'content': system_instruction},
+                      {'role': 'user', 'content': "hey"}],
+        ).choices[0].message.content
+
+    st.session_state.messages = [{'role': 'system', 'content': system_instruction},
+                                 {'role': 'assistant', 'content': openingszin}]
+
 
 for message in st.session_state.messages:
     if message['role'] != 'system':
       with st.chat_message(message["role"]):
         st.markdown(message["content"])
 
-if prompt := st.chat_input("What is up?"):
+
+
+if prompt := st.chat_input("..."):
     st.session_state.messages.append({"role": "user", "content": prompt})
     with st.chat_message("user"):
         st.markdown(prompt)
 
     with st.chat_message("assistant"):
-        stream = client.chat.completions.create(
+        stream = st.session_state.client.chat.completions.create(
             model="x-ai/grok-4-fast:free",
-            messages=[
+            messages=[{'role': 'system', 'content': f"Huidig profiel: {st.session_state.profiel}"}] + [
                 {"role": m["role"], "content": m["content"]}
                 for m in st.session_state.messages
             ],
@@ -67,64 +92,22 @@ if prompt := st.chat_input("What is up?"):
     st.session_state.messages.append({"role": "assistant", "content": response})
 
 
-# Create a client.
-#client = genai.Client(api_key = google_key)
-#if "chat" not in st.session_state:
-#    st.session_state.chat = client.chats.create(model="gemini-2.5-flash",
-#                      config=types.GenerateContentConfig(
-#                system_instruction="""Je bent een vriendelijke chatbot in Vlaanderen die een kort en informeel startgesprek heeft met de gebruiker voordat de gebruiker start met de leermodules over AI.
-#                    Pas je wat betreft de taal aan aan de gebruiker. 
-#                    Je doel is om snel een beeld te vormen van de gebruiker, zonder dat het voelt als een interview. 
-#                    Stuur het gesprek naar:
-#                    - Leeftijd en opleidingsniveau (bijv. lager, middelbaar, universitair).
-#                    - Taalvaardigheid (moedertaal, voorkeur voor instructietaal).
-#                    - Reden om deel te nemen (werkgerelateerd, verplicht voor studie, persoonlijke interesse).
-#                    - Digitale geletterdheid (beginner, gemiddeld, gevorderd).
-#                    - Voorkeursleerstijl (tekst lezen, video, audio/podcast, interactieve oefeningen).
-#                    - Huidige kennisniveau van AI (geen, basis, gemiddeld, gevorderd).
-#                    - Ervaring met AI-tools (bijv. ChatGPT, DALL·E, Midjourney, Copilot, AI in MS Office/Google).
-#                    - Houding tegenover AI: nieuwsgierig, sceptisch, kritisch, enthousiast.
-#                    - Specifieke interesses (Praktisch gebruik, Technisch, Ethisch/juridisch, Toekomst, ...)
-#
-#                    Hou je reacties kort, vriendelijk en menselijk. 
-#                    Vermijd formele testvragen of opsommingen; laat het voelen als een natuurlijk gesprek.
-#                    Vraag niet te hard door, ga op tijd over naar een ander onderwerp."""))
-
-# Create a session state variable to store the chat messages. This ensures that the
-# messages persist across reruns.
-
-# Display the existing chat messages via `st.chat_message`.
-#for message in st.session_state.chat.get_history():
-#    with st.chat_message(message.role):
-#        st.markdown(message.parts[0].text)
-
-# Create a chat input field to allow the user to enter a message. This will display
-# automatically at the bottom of the page.
-#if prompt := st.chat_input("..."):
-
-    # Store and display the current prompt.
-#    with st.chat_message("user"):
-#        st.markdown(prompt)
-
-    # Generate a response using the OpenAI API.
-#    response = st.session_state.chat.send_message(prompt)
-#    with st.chat_message("model"): #assistant
-#        st.markdown(response.text)
 
 
-client_profile = genai.Client(api_key = google_key)
 if st.session_state.messages:
     prompt = f"""Maak op basis van het CHATGESPREK een beknopt profiel aan van de gebruiker. 
     Indien er geen informatie is geef dan onbekend aan.
+    Geef in het profiel de mogelijkheden tussen de haakjes NIET weer.
     Concentreer je op volgende informatie:
+    - Aanspreking (naam, bijnaam).
     - Leeftijd en opleidingsniveau (bijv. lager, middelbaar, universitair).
-    - Taalvaardigheid (moedertaal, voorkeur voor instructietaal).
-    - Reden om deel te nemen (werkgerelateerd, verplicht voor studie, persoonlijke interesse).
+    - Taal (moedertaal, voorkeur voor instructietaal).
+    - Reden om deel te nemen (werkgerelateerd, voor studie, persoonlijke interesse).
     - Digitale geletterdheid (beginner, gemiddeld, gevorderd).
     - Voorkeursleerstijl (tekst lezen, video, audio/podcast, interactieve oefeningen).
-    - Huidige kennisniveau van AI (geen, basis, gemiddeld, gevorderd).
+    - Kennis van AI (geen, basis, gemiddeld, gevorderd).
     - Ervaring met AI-tools (bijv. ChatGPT, DALL·E, Midjourney, Copilot, AI in MS Office/Google).
-    - Houding tegenover AI: nieuwsgierig, sceptisch, kritisch, enthousiast.
+    - Houding tegenover AI (nieuwsgierig, sceptisch, kritisch, enthousiast).
     - Specifieke interesses (Praktisch gebruik, Technisch, Ethisch/juridisch, Toekomst, ...)
 
     CHATGESPREK:
@@ -132,9 +115,16 @@ if st.session_state.messages:
 
     Profiel:
     """
-    print(prompt)
-    response = client_profile.models.generate_content(
-        model="gemini-2.5-flash",
-        contents=prompt
+    response = st.session_state.client.chat.completions.create(
+        model="x-ai/grok-4-fast:free",
+        messages=[
+        {
+          "role": "user",
+          "content": prompt
+        }
+        ]
     )
-    st.session_state.profiel = response.text
+    st.session_state.profiel = response.choices[0].message.content
+
+    with st.sidebar:
+        st.markdown(st.session_state.profiel)
--- pages/1_🌍_Ecologie.py
+++ pages/1_🌍_Ecologie.py
@@ -1,59 +1,92 @@
 import streamlit as st
 import json
-from google import genai
 import os
-from google.genai import types
-
-st.markdown("# 🌍Ecologie")
-
-with open('ecologie.json') as json_file:
-    inhoud = dict(json.load(json_file))
-
-inhoud_values = list(inhoud.values())
-
-st.write(inhoud_values[0])
-
-# openai_api_key = st.text_input("OpenAI API Key", type="password")
-google_key = os.getenv('GOOGLE_API')
-
-# Create a client.
-client = genai.Client(api_key = google_key)
-
-if "chat_ecol" not in st.session_state:
-    st.session_state.chat_ecol = client.chats.create(model="gemini-2.5-flash",
-                      config=types.GenerateContentConfig(
-                system_instruction=inhoud_values[0]))
-
-# Display the existing chat messages via `st.chat_message`.
-for message in st.session_state.chat_ecol.get_history():
-    with st.chat_message(message.role):
-        st.markdown(message.parts[0].text)
-
-# Create a chat input field to allow the user to enter a message. This will display
-# automatically at the bottom of the page.
-if prompt := st.chat_input("..."):
-
-    # Store and display the current prompt.
-    with st.chat_message("user"):
-        st.markdown(prompt)
-
-    # Generate a response using the OpenAI API.
-    response = st.session_state.chat_ecol.send_message(prompt)
-    with st.chat_message("model"): #assistant
-        st.markdown(response.text)
-
+import requests
+from openai import OpenAI
+from huggingface_hub import InferenceClient
 
 st.sidebar.header("🌍Ecologie")
+
+def radio_action():
+    st.session_state.progress = 0
+
 
 with st.sidebar:
     module = st.radio(
     "**Modules**",
     ["Introductie", "Basisbegrippen", "Milieu-impact"],
-    key="modul"
-    )
+    key="modul",
+    on_change=radio_action)
 
     if "profiel" in st.session_state:
         st.markdown(st.session_state.profiel)
+
+
+
+st.markdown("# 🌍Ecologie")
+
+if "profiel" not in st.session_state:
+    st.session_state.profiel = ""
+
+if "client" not in st.session_state:
+  OR_key = os.getenv('OPEN_ROUTER_API')
+  st.session_state.client = OpenAI(
+    base_url="https://openrouter.ai/api/v1",
+    api_key=OR_key
+  )
+
+#if st.button("Afbeelding"):
+#    HF_TOKEN = os.environ["HF_TOKEN"]
+#    client = InferenceClient(
+#      provider="fal-ai",
+#      api_key=HF_TOKEN)
+#    image = client.text_to_image(
+#      "Astronaut riding a horse",
+#      model="tencent/HunyuanImage-2.1")
+#    st.image(image)
+
+if "progress" not in st.session_state:
+    st.session_state.progress = 0
+
+
+with open('ecologie.json') as json_file:
+    inhoud = dict(json.load(json_file))
+
+st.write(f"# {module} - {list(inhoud.keys())[st.session_state.progress]}")
+
+if st.button("➡️ Volgende ➡️"):
+    st.session_state.progress += 1
+
+inhoud_values = list(inhoud.values())
+
+if "messages_ec" not in st.session_state:
+    system_instruction=f"""Je bent een online leercoach die een introductie presenteert voor een leermodule over kunstmatige intelligentie (AI) en de ecologische voetafdruk. 
+      Je schrijft in een toegankelijke, motiverende en inspirerende stijl, direct gericht aan de deelnemer (‘je/jij’). 
+      Gebruik korte alinea’s, opsommingen waar passend, en zorg dat de tekst vlot en begrijpelijk is. 
+      Elke sectie moet helder afgebakend zijn met een titel of emoji.
+      
+      Pas de boodschap aan op basis van het PROFIEL van de gebruiker.
+      PROFIEL: {st.session_state.profiel}"""
+
+    st.session_state.messages_ec = [{'role': 'system', 'content': system_instruction}]
+
+prompt = f"""
+{inhoud_values[st.session_state.progress]}
+"""
+st.session_state.messages_ec.append({"role": "user", "content": inhoud_values[st.session_state.progress]})
+
+completion = st.session_state.client.chat.completions.create(
+  model="x-ai/grok-4-fast:free",
+  messages=[{"role": m["role"], "content": m["content"]}
+                for m in st.session_state.messages_ec
+            ],
+  stream=True,
+)
+
+response = st.write_stream(completion)
+
+st.session_state.messages_ec.append({"role": "assistant", "content": response})
+
 
 
 
